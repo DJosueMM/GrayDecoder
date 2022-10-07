513601f0f8e114eefe1e2fe46f91ee7267ebcf5c
DJosueMM <djosuemm@gmail.com>
2022-10-06 22:37:51 -0600

    Subsubmodulos finales

--- SubModulo 7 segmentos/Sub_submodulos/Detector_decenas.sv
+++ SubModulo 7 segmentos/Sub_submodulos/Detector_decenas.sv
@@ -1,42 +1,18 @@
 `timescale 1ns / 1ps
 
-module Control_displays(input logic d,
-                        input logic clk, 
-                        input logic d_timer,
-					    output logic En_unidad,
-					    output logic En_decena );
+module Detector_Decenas(input logic [3:0]in,
+					    output logic digito);
 
-    // Salida del bloque combinacional
-    logic unidad;   
-    logic decena;
+    logic [3:0]Dbin_in; //Entrada al bloque combinacional
+    logic d;
+    
 
-    assign En_unidad = unidad;
-    assign En_decena = decena;
-    
-    //FSM
-    
-    typedef enum logic {S0,S1} statetype;
-    statetype state, nextstate;
-    
-    // Registro de estado
-    always_ff@(posedge clk)
-        if(d_timer)  state <= nextstate;
-    
-    //Logica de siguiente estado
-    always_comb
-        case(state)
-            S0: if(d) nextstate = S1;
-                else  nextstate = S0;
-            S1: nextstate = S0;
-        endcase
-    
-    //Lógica de salida
-    always_comb
-        case(state)
-            S0: unidad = 0;
-            S0: decena = 1;
-            S1: unidad = 1;
-            S1: decena = 0;
-        endcase
-    
+    assign Dbin_in[3:0] = in[3:0]; 
+    assign digito = d;
+
+    //Detección de decenas
+ 
+    assign d  =  Dbin_in[3] & Dbin_in[2] |  Dbin_in[3] & Dbin_in[1];
+        
+ 
 endmodule
